98eebccfa1447929a4b13a23719d2da5d1a733fa
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-09-12 21:38:18 +0900

    eslintを追加

--- nextjs-gihyo-book/src/pages/_app.tsx
+++ nextjs-gihyo-book/src/pages/_app.tsx
@@ -22,14 +22,18 @@ const GlobalStyle = createGlobalStyle`
   ol, ul {
     list-style: none;
   }
-`
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta key="charset" name="charset" content="utf-8" />
-        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
         <meta property="og:locale" content="ja_JP" />
         <meta property="og:type" content="website" />
       </Head>
@@ -39,4 +43,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default MyApp;
